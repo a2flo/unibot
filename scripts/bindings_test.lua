e69ec4426b14e5087c56d392f6d6a43e47cade1d
Florian Ziesche <florian.ziesche@gmail.com>
2010-02-23 22:32:17 +0100

    * send bindings_test messages to target (instead of channel), except for send_channel_msg test

--- scripts/bindings_test.lua
+++ scripts/bindings_test.lua
@@ -10,37 +10,37 @@ function handle_message(origin, target, cmd, parameters)
 		local channel = get_config_entry("channel")
 		
 		-- net
-		send("PRIVMSG "..channel.." :send test")
+		send("PRIVMSG "..target" :send test")
 		send_channel_msg("send_channel_msg test")
-		send_private_msg(channel, "send_private_msg test")
+		send_private_msg(target, "send_private_msg test")
 		send_kick("tester", "42")
 		local host_ip, host_port = get_local_ip()
 		local server_ip, server_port = get_server_ip()
-		send_channel_msg("local: "..host_ip..":"..host_port..", server: "..server_ip..":"..server_port)
+		send_private_msg(target, "local: "..host_ip..":"..host_port..", server: "..server_ip..":"..server_port)
 		
 		-- bot states
 		local silenced = get_bot_state("silenced")
 		set_bot_state("silenced", xor(silenced, true))
 		local new_silenced = get_bot_state("silenced")
-		send_channel_msg("bot state test: "..tostring(silenced)..", "..tostring(new_silenced))
+		send_private_msg(target, "bot state test: "..tostring(silenced)..", "..tostring(new_silenced))
 		
 		-- config
 		local owners = get_config_entry("owner_names")
-		send_channel_msg("config test, owner names: "..owners)
+		send_private_msg(target, "config test, owner names: "..owners)
 		
 		local is_owner1 = is_owner("[flo]")
 		local is_owner2 = is_owner("tester")
-		send_channel_msg("config test, is owner: [flo]: "..tostring(is_owner1)..", tester: "..tostring(is_owner2))
+		send_private_msg(target, "config test, is owner: [flo]: "..tostring(is_owner1)..", tester: "..tostring(is_owner2))
 		
 		-- misc
 		local test_url = "http://www.google.com/?!? blah blubb /";
-		send_channel_msg("url: "..test_url..", encoded: "..url_encode(test_url))
-		send_channel_msg("prev msg: "..get_prev_msg(1))
+		send_private_msg(target, "url: "..test_url..", encoded: "..url_encode(test_url))
+		send_private_msg(target, "prev msg: "..get_prev_msg(1))
 		
 		local sc_str = "s,t.r;i:n!g w?/^ \"special\'chars!"
-		send_channel_msg("strip_special_chars: "..strip_special_chars(sc_str))
+		send_private_msg(target, "strip_special_chars: "..strip_special_chars(sc_str))
 		
-		send_channel_msg("handle_args_chronological: "..handle_args_chronological("-1"))
+		send_private_msg(target, "handle_args_chronological: "..handle_args_chronological("-1"))
 	end
 	return 0
 end
