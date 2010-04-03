944be3b5bf89c48c39aabfbf7d9368ab4e2581b2
Florian Ziesche <florian.ziesche@gmail.com>
2010-04-03 19:40:02 +0200

    * added premake.sh script, to easily call premake with your platform information  * added INSTALL file ("how to build unibot")

---
+++ premake.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+UNIBOT_OS="unknown"
+UNIBOT_PLATFORM="x32"
+UNIBOT_MAKE_PLATFORM="32"
+
+case $( uname ) in
+	"Darwin")
+		UNIBOT_OS="macosx"
+		;;
+	"Linux")
+		UNIBOT_OS="linux"
+		;;
+	[a-zA-Z0-9]*"BSD")
+		UNIBOT_OS="bsd"
+		;;
+	*)
+		echo "unknown operating system - exiting"
+		exit
+		;;
+esac
+
+
+case $( uname -m ) in
+	"i386"|"i486"|"i586"|"i686")
+		UNIBOT_PLATFORM="x32"
+		UNIBOT_MAKE_PLATFORM="32"
+		;;
+	"x86_64"|"amd64")
+		UNIBOT_PLATFORM="x64"
+		UNIBOT_MAKE_PLATFORM="64"
+		;;
+	*)
+		echo "unknown architecture - using "${UNIBOT_PLATFORM}
+		exit;;
+esac
+
+
+echo "using: premake4 --cc=gcc --os="${UNIBOT_OS}" gmake"
+
+premake4 --cc=gcc --os=${UNIBOT_OS} gmake
+
+echo ""
+echo "###################################################"
+echo "# NOTE: use 'make config=release"${UNIBOT_MAKE_PLATFORM}"' to build unibot"
+echo "###################################################"
+echo ""
