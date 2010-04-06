c3ecc3249d62d525b7279028e48c10d33c67c755
Yannic Haupenthal <mail@thedarkwebsite.de>
2010-04-06 20:16:08 +0200

    add2file.lua and folder output added (root only)

---
+++ scripts/add2file.lua
@@ -0,0 +1,13 @@
+
+-- lua add something to a file
+
+function handle_message(origin, target, cmd, parameters)
+        if is_owner(origin) and cmd == "add" then
+                local name = "output/outputfile-"..os.time()
+                io.open(name, a)
+                io.output(name)
+                io.write(parameters)
+                io.close()
+        end
+        return 0
+end
