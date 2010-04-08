f3bccae2104e117d0ad4ba83e4f29b7e4091a1f5
Yannic Haupenthal <mail@thedarkwebsite.de>
2010-04-08 18:41:22 +0200

    roulette: user can also use roulette, but only with itself as name and it's now really random

--- scripts/roulette.lua
+++ scripts/roulette.lua
@@ -3,19 +3,20 @@
 
 function handle_message(origin, target, cmd, parameters)
 	if cmd == "roulette" then
-		local name = ( parameters ~= cmd ) and parameters or origin
-		-- alternative: if name is owner and origin no owner, then the origin will be kicked.
+		local name = origin
 		if is_owner(origin) then
-			send_private_msg(target, "Fire!")
-			if ((math.mod(math.random(0, 1000), 42)) <= 21) then
-				send_private_msg(target, "Boom!")
-				-- TODO random message
-				send_kick(name, "Bad luck!")
-			else
-				send_private_msg(target, "Click!")
-			end
+			name = ( parameters ~= cmd ) and parameters or origin
 		else
-			send_kick(origin, "Permission denied.")
+			name = origin
+		-- alternative: if name is owner and origin no owner, then the origin will be kicked.
+		send_private_msg(target, "Fire!")
+		-- if ((math.mod(math.random(0, 1000), 42)) <= 21) then
+		if (math.random(0, 1000) <= 500) then
+			send_private_msg(target, "Boom!")
+			-- TODO random message
+			send_kick(name, "Bad luck!")
+		else
+			send_private_msg(target, "Click!")
 		end
 	end
 	return 0
