66dee0ea23f7f036a16203bfa5b6f43037f02e8b
Yannic Haupenthal <mail@thedarkwebsite.de>
2010-04-08 23:38:18 +0200

    ping: added pong command

--- scripts/ping.lua
+++ scripts/ping.lua
@@ -5,5 +5,8 @@ function handle_message(origin, target, cmd, parameters)
 	if cmd == "ping" then
 		send_private_msg(target, "pong!")
 	end
+	if cmd == "pong" then
+		send_private_msg(target, "ping!")
+	end
 	return 0
 end
