42b955ddbc37ba6e5debaf17fc085207bbe1306f
Yannic Haupenthal <mail@thedarkwebsite.de>
2010-04-10 21:59:37 +0200

    add2file: unexpand :)

--- scripts/add2file.lua
+++ scripts/add2file.lua
@@ -2,12 +2,12 @@
 -- lua add something to a file
 
 function handle_message(origin, target, cmd, parameters)
-        if is_owner(origin) and cmd == "add" then
-                local name = "output/outputfile-"..os.time()
-                io.open(name, a)
-                io.output(name)
-                io.write(parameters)
-                io.close()
-        end
-        return 0
+	if is_owner(origin) and cmd == "add" then
+		local name = "output/outputfile-"..os.time()
+		io.open(name, a)
+		io.output(name)
+		io.write(parameters)
+		io.close()
+	end
+	return 0
 end
