73c52ad50019da4dbc9e12b605ccc1650b204e62
Yannic Haupenthal <mail@thedarkwebsite.de>
2010-04-13 21:30:55 +0200

    mensa: fixed urls and added saturday

--- scripts/mensa.lua
+++ scripts/mensa.lua
@@ -4,11 +4,11 @@
 function handle_message(origin, target, cmd, parameters)
 	if cmd == "mensa" then
 		local day = tonumber(os.date("%w", os.time()))
-		local days = { "montag", "dienstag", "mittwoch", "donnerstag", "freitag" }
-		if day >= 1 and day <= 5 then
-			send_private_msg(target, "http://www.studentenwerk-saarland.de/seiten/verpflegung/speiseplan_sbr/"..days[day])
+		local days = { "montag", "dienstag", "mittwoch", "donnerstag", "freitag", "samstag" }
+		if day >= 1 and day <= 6 then
+			send_private_msg(target, "http://www.studentenwerk-saarland.de/seiten/verpflegung/speiseplan_sbr/"..days[day]..".htm")
 		else
-			send_private_msg(target, "Es ist Wochenende! Steh auf und kauf dir was ;)")
+			send_private_msg(target, "Es ist Wochenende! Steh auf und koch dir was ;)")
 		end
 	end
 	return 0
