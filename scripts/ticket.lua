34497e005843cee9e4135483373221ad4133e34e
Florian Ziesche <florian.ziesche@gmail.com>
2010-04-15 00:52:04 +0200

    * print ticket or bug report dependent on cmd/type

--- scripts/ticket.lua
+++ scripts/ticket.lua
@@ -7,7 +7,8 @@ function handle_message(origin, target, cmd, parameters)
 	if cmd == "ticket" or cmd == "bug" then
 		local summary = url_encode(( parameters ~= cmd ) and parameters or "<please specify a short summary>")
 		local ticket_type = ( cmd == "ticket" ) and "new%20functionality" or "defect"
-		send_private_msg(target, "Please open this link to send your ticket: http://trac-git.assembla.com/unibot/newticket?reporter="..origin.."&summary="..summary.."&type="..ticket_type.."&version=0.2.x&milestone=v0.2")
+		local ticket_type_str = ( cmd == "ticket" ) and "ticket" or "bug report"
+		send_private_msg(target, "Please open this link to send your "..ticket_type_str..": http://trac-git.assembla.com/unibot/newticket?reporter="..origin.."&summary="..summary.."&type="..ticket_type.."&version=0.2.x&milestone=v0.2")
 	end
 	return 0
 end
