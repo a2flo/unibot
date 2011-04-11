ac6309f3cc527573723662d3a6d16c6be694ed45
Florian Ziesche <florian.ziesche@gmail.com>
2011-04-11 17:27:50 +0200

    * updated scripts

--- scripts/mensa.lua
+++ scripts/mensa.lua
@@ -6,9 +6,8 @@ require "global"
 function handle_message(origin, target, cmd, parameters)
 	if cmd == "mensa" then
 		local day = tonumber(os.date("%w", os.time()))
-		local days = { "montag", "dienstag", "mittwoch", "donnerstag", "freitag", "samstag" }
 		if day >= 1 and day <= 6 then
-			send_private_msg(target, "http://www.studentenwerk-saarland.de/seiten/verpflegung/speiseplan_sbr/"..days[day]..".htm")
+			send_private_msg(target, "http://www.studentenwerk-saarland.de/de/Essen/Essen-in-Saarbrucken/Speiseplan-aktuell")
 		else
 			send_private_msg(target, "Es ist Wochenende! Steh auf und koch dir was ;)")
 		end
--- scripts/upload.lua
+++ scripts/upload.lua
@@ -5,7 +5,6 @@ require "global"
 
 function handle_message(origin, target, cmd, parameters)
 	if cmd == "upload" then
-		--send_private_msg(target, "http://www.file-upload.net/")
 		send_private_msg(target, "http://www.megaupload.com")
 	end
 	return 0
