1d20ce18bab4d885e2353cf6281a26b038954292
Yannic Haupenthal <mail@thedarkwebsite.de>
2011-05-01 21:52:34 +0200

    unikram fixed

--- scripts/unikram.lua
+++ scripts/unikram.lua
@@ -3,7 +3,7 @@
 
 function handle_message(origin, target, cmd, parameters)
 	if cmd == "unikram" then
-		send_private_msg(target, "http://thedarkwebsite.de/yannic/?s=unikram")
+		send_private_msg(target, "http://thedarksite.org/yannic/unikram.htm")
 	end
 	return 0
 end
