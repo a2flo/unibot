8b255e78763125e7ba5fd3b414c5b37c6d5fd694
Yannic Haupenthal <mail@thedarkwebsite.de>
2014-03-15 17:37:22 +0100

    * added TODOs * fixed security issue

--- scripts/fortune.lua
+++ scripts/fortune.lua
@@ -1,10 +1,12 @@
 
 -- !fortune
+-- TODO: don't update the topic within 48-72 hours, if the topic was set by someone else (?)
+-- TODO: do this within the core, so the topic can change automatically every 24 hours
 
 require "global"
 
 function handle_message(origin, target, cmd, parameters)
-	if cmd == "fortune" then
+	if is_owner(origin) and cmd == "fortune" then
 		fortune = execute_command("fortune -s | tr '\\n' ' ' | tr '\\t' ' ' | sed 's/ \\+/ /g' 2>&1")
 		channel = get_config_entry("channel")
 		send("TOPIC "..channel.." :"..fortune)
